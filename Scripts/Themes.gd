717196294f7c18a34beff5a55ca2ed832000d904
Laserology <admin@laserology.net>
2025-07-10 10:49:11 -0700

    Begin adding theme support

---
+++ Scripts/Themes.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var ColorTerracotta: Color = Color.from_string("#16030b", Color.BLACK)
+var ColorNormal: Color = Color.from_string("#1e1e2b", Color.BLACK)
+
+func Apply(ThemeName: String) -> void:
+	pass
